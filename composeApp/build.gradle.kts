39f41ba31ff6dca79e1bb45cca8e23b6d67ec73d
rowanwhall <rhall1705@gmail.com>
2026-03-22 17:44:33 -0400

    version bumps for 1.1 beta

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -53,8 +53,8 @@ android {
         applicationId = "com.arcvgc.app"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 2
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.1"
     }
     packaging {
         resources {
